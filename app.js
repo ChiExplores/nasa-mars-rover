93ffb40974dbf6289f17de9c887dfb358fe50a35
Chi Huynh <contact@chihuynh.me>
2020-02-24 13:55:45 -0800

    set up environment variables

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const accountSid = process.env.TWILIO_SID;
-const authToken = process.env.TWILIO_TOKEN;
+const dotenv = require('dotenv');
+dotenv.config();
+const env = process.env
+const accountSid = env.TWILIO_SID
+const authToken = env.TWILIO_TOKEN
 const client = require("twilio")(accountSid, authToken);
-const nasaAPI = process.env.NASA_API;
-const fetch = require("node-fetch");
+const nasaAPI = env.NASA_AUTH;
+// const fetch = require("node-fetch");
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = env.PORT;
 
 
-client.messages
-	.create({
-		body: 'Hello! This is nasa mars rover',
-		from: process.env.number,
-		to: process.env.cell
-	})
-	.then(message => console.log(message.sid));
-// const server = http.createServer((req, res) => {
-// 	res.statusCode = 200;
-// 	res.setHeader('Content-Type', 'text/plain');
-// 	res.end('Hello World');
-// });
+// client.messages
+// 	.create({
+// 		body: 'HELLO, THIS IS CHI',
+// 		from: +15744018284,
+// 		to: +4159177690
+// 	})
+// 	.then(message => console.log(message.sid));
 
-// server.listen(port, hostname, () => {
-// 	console.log(`Server running at http://${authToken}:${port}/`);
-// });
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
+console.log(`${port}`)
