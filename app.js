233912c2fae89db89c26af552c64671cd532774e
Chi Huynh <contact@chihuynh.me>
2020-03-10 16:48:59 -0700

    added html page

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const env = process.env
-const accountSid = env.TWILIO_SID
-const authToken = env.TWILIO_TOKEN
-const client = require("twilio")(accountSid, authToken);
-const nasaAPI = env.NASA_AUTH;
-const fetch = require("node-fetch");
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const env = process.env
+// const accountSid = env.TWILIO_SID
+// const authToken = env.TWILIO_TOKEN
+// const client = require("twilio")(accountSid, authToken);
+const nasaAPI = "RShnld8C4Xa3MHfwMENxQq70tF1cezlzbFhxILnG";
+// const fetch = require("node-fetch");
 let img = void 0;
+let camera = ['fhaz',];
+let rovers = ['curiosity', 'spirit', 'opportunity', 'perserverance'];
 
-async function getMarsRoverAsync() {
-	let response = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=DEMO_KEY ');
+//Try doing it promises 
+
+
+
+
+// getMarsRoverAsync()
+// 	.then(data => sendMsg(data.photos[0].img_src))
+
+// sendMsg = (data) => {
+// 	client.messages
+// 		.create({
+// 			body: 'HELLO, THIS IS IMAGE FROM MARS ROVER',
+// 			from: env.TWILIO_NUMBER,
+// 			mediaUrl: [data],
+// 			to: +4146781466
+// 		})
+
+
+
+let getPhoto = async function getMarsRoverAsync(e) {
+	let roverName = e.target.id
+	console.log(e.target.id)
+	let response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=1001&camera=fhaz&api_key=${nasaAPI}`);
 	let data = await response.json()
-	return data;
+	// return data;
+	console.log(data)
 }
 
-getMarsRoverAsync()
-	.then(data => sendMsg(data.photos[0].img_src))
-
-sendMsg = (data) => {
-	client.messages
-		.create({
-			body: 'HELLO, THIS IS IMAGE FROM MARS ROVER',
-			from: +'',
-			mediaUrl: [data],
-			to: +''
-		})
-		.then(message => console.log(message.sid));
-}
-
+document.getElementById("spirit").addEventListener("click", getPhoto);
+document.getElementById("curiosity").addEventListener("click", getPhoto);
+document.getElementById("opportunity").addEventListener("click", getPhoto);
